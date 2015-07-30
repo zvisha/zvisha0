e44b9366c4063f3be3e8e0dc2bbdc4bd28e2145f
zvshte <zvisha@gmail.com>
2015-07-30 16:09:54 +0300

    1

---
+++ server.js
@@ -0,0 +1,10 @@
+/**
+ * Created by zvshte on 7/30/2015.
+ */
+
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
