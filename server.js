e792a1b508b258bcd0804118e12a657d3064f2ac
zvshte <zvisha@gmail.com>
2015-08-04 13:43:52 +0300

    1

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ var http = require('http');
 var port = process.env.PORT || 888;
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Zvisha oman node.js, random is ' + (Math.random()*100).toFixed());
+    res.end('Zvisha oman 2 node.js, random is ' + (Math.random()*100).toFixed());
 }).listen(port);
