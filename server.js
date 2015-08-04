d571ffa9c8045a23257b7c9ade5b4436872102c8
zvshte <zvisha@gmail.com>
2015-08-04 13:30:14 +0300

    1

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
  * Created by zvshte on 7/30/2015.
  */
 
-var http = require('http')
-var port = process.env.PORT || 1337;
+var http = require('http');
+var port = process.env.PORT || 888;
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Zvisha oman node.js\n');
+    res.end('Zvisha oman node.js, random is ' + (Math.random()*100).toFixed());
 }).listen(port);
